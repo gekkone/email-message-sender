28deafb3b3158a99856b961d0d1ad169d3b918a6
Gekkone <sergej.soloyov@yandex.ru>
2020-09-21 13:19:32 +0300

    Реализована возможность подключение recaptcha на стороне фронта Добавлены соответствующие конфигурации В примере файла переменных окружения добавлены ключи recaptcha для локальной разработки (только localhost)

---
+++ resources/views/includes/recaptcha.blade.php
@@ -0,0 +1,4 @@
+@if (config('app.validate_recaptcha'))
+    <meta name="recaptcha-key" content="{{ config('secrets.recaptcha.site_key') }}">
+    <script src="https://www.google.com/recaptcha/api.js?render={{ config('secrets.recaptcha.site_key') }}"></script>
+@endif
